69af428a696b8227021816458d8ad24f9c42f5a9
ANDERSON KAPPS BRAGA EULALIO <PET0159803@aluno.firjansenaisesi.com.br>
2025-07-25 20:39:07 -0300

    ajustando erros

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -2,10 +2,17 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 
 // Cria o Contexto
 const ThemeContext = createContext();
+const isDarkMode = localStorage.getItem("darkMode");
 
 // Cria o componente Provedor
 export function ThemeProvider({ children }) {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(()=>{
+    const isDark = localStorage.getItem("darkMode");
+    if(isDark === "dark"){
+      return true
+    }
+    return false
+  });
 
   // Efeito que roda toda vez que 'darkMode' muda
   useEffect(() => {
@@ -14,8 +21,10 @@ export function ThemeProvider({ children }) {
     // Adiciona ou remove a classe do body
     if (darkMode) {
       body.classList.add("dark-mode");
+      localStorage.setItem("darkMode", "dark")
     } else {
       body.classList.remove("dark-mode");
+      localStorage.setItem("darkMode", "light")
     }
   }, [darkMode]);
 
